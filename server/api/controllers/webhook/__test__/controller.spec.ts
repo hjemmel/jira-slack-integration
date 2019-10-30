1352de733fc53da47a8918551c3d16865e1720d1
Hans Jakob Emmel <hans.emmel@gmail.com>
2019-10-30 21:40:55 +1300

    dockerizing my app

--- server/api/controllers/webhook/__test__/controller.spec.ts
+++ server/api/controllers/webhook/__test__/controller.spec.ts
@@ -1,15 +1,15 @@
-import request from 'supertest';
-import Server from '../../../..';
+import request from "supertest";
+import Server from "../../../..";
 
-describe('Webhook', () => {
-    it('should get all examples', (done) =>
-        request(Server)
-            .post('/api/v1/webhook', )
-            .send({ name: 'tobi' })
-            .then(r => {
-                expect(r.body).toBeInstanceOf(Array);
-                expect(r.status).toBe(400);
-                expect(r.body).toHaveLength(2);
-                done()
-            }));
+describe("Webhook", () => {
+  it("should get all examples", done =>
+    request(Server)
+      .post("/api/v1/webhook")
+      .send({ name: "tobi" })
+      .then(r => {
+        expect(r.body).toBeInstanceOf(Array);
+        expect(r.status).toBe(400);
+        expect(r.body).toHaveLength(2);
+        done();
+      }));
 });
